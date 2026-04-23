0240bb28c45081a666a30e6e7a5670ca6d4f0944
yongsky1025 <qaz7984@gmail.com>
2026-04-23 14:58:42 +0900

    back,frontend 환경설정 완료

---
+++ sub-backend/src/main/java/com/soldesk/sub_backend/SubBackendApplication.java
@@ -0,0 +1,13 @@
+package com.soldesk.sub_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubBackendApplication.class, args);
+	}
+
+}
---
+++ sub-backend/src/test/java/com/soldesk/sub_backend/SubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soldesk.sub_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
